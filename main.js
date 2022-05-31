4aec11c2ac0b854fe9f24bf5dd7776edcdf928e7
Vishwajeetsinh Rathod <vishwajeetsinh.arham@gmail.com>
2022-05-31 17:24:38 +0530

    update index by 1 if update value is less than goal value

--- main.js
+++ main.js
@@ -23,6 +23,9 @@
     let heightGoalAfter = ''
 
 
+    let new2 = null
+
+
     // first split the selector value
 
     for (let i=1; i<selector.length-1; i++){
@@ -36,13 +39,28 @@
         selectorMiddleValueArray.push(selectorMiddleValue)
 
         selectorSplittedHeight = selectorMiddleValue[0]
-        selectorSplittedHeightArray.push(selectorSplittedHeight)
+        selectorSplittedHeightArray.push(parseInt(selectorSplittedHeight))
 
         selectorSplittedWidth = selectorMiddleValue[1]
-        selectorSplittedWidthArray.push(selectorSplittedWidth)
+        selectorSplittedWidthArray.push(parseInt(selectorSplittedWidth))
         }
-        console.log(valueWithXArray)
+        // console.log(valueWithXArray)
+        
+        // FIND THE MAX FROM THE ARRAY    H E I G H T   
+        
+        let arrH = selectorSplittedHeightArray
+        let indexesOfsameH = []
+        let sameHcount = 0
+        // console.log(maxOfH)
+        // let indexesOfMaxH = []
 
+        // for (let i=0; i<arrH.length; i++){
+        //     if(arrH[i] === maxOfH){
+        //         indexesOfMaxH.push(i)
+        //     }
+        // }
+        // console.log(indexesOfMaxH)
+        // console.log(arrH.findIndex(arrH => arrH == 0))
 
 
 
@@ -56,7 +74,7 @@
             heightGoal = height.value
             let heightGoalBefore = heightGoal
 
-            console.log('heightGoal Before ', heightGoal)
+            // console.log('heightGoal Before ', heightGoal)
 
             var heightClosest =heightCounts.reduce(function(prev, curr) {
                 return (Math.abs(curr - heightGoal) < Math.abs(prev - heightGoal) ? curr : prev);
@@ -64,14 +82,21 @@
 
             heightGoalAfter = parseInt(heightClosest)
 
-            console.log('heightgoal after' ,heightClosest)
+            // console.log('heightgoal after' ,heightClosest)
 
 
-            let ifHeightISLess = heightGoalBefore - heightGoalAfter
+            let ifHeightISLess = heightGoalAfter -   heightGoalBefore 
 
             if( ifHeightISLess > 0){
-                console.log('updated value of Height is smaller than needed',  ifHeightISLess)
+                // console.log('updated value of Height is smaller than needed',  ifHeightISLess)
+                // console.log('before height closest')
+                // indexOfHeight = selectorSplittedHeightArray.indexOf(heightClosest) + 1
+                // updatedHeight = selectorSplittedHeightArray.at(indexOfHeight)
+                // heightClosest = updatedHeight
+                // console.log('updated value of height is ', updatedHeight)
+
             }
+            console.log(ifHeightISLess, 'ifheightis l e s  s')
 
 
             //  for the width 
@@ -80,9 +105,10 @@
             widthGoal = width.value
             let widthGoalBefore = widthGoal
 
-            console.log('widthGoal Before ', widthGoalBefore)
-
+            // console.log('widthGoal Before ', widthGoalBefore)
+//180
             var widthClosest =widthCounts.reduce(function(prev, curr) {
+                // console.log("widthGoal=>"+widthGoal+" curr"+curr+" prev"+prev);
                 return (Math.abs(curr - widthGoal) < Math.abs(prev - widthGoal) ? curr : prev);
             });
 
@@ -91,10 +117,18 @@
             console.log('widthgoal after' ,widthGoalAfter)
 
 
-            let ifWidthISLess = widthGoalBefore - widthGoalAfter
+            let ifWidthISLess =   widthGoalBefore - widthGoalAfter
 
             if( ifWidthISLess > 0){
-                console.log('updated value Of width  is smaller than needed',  ifWidthISLess)
+                // console.log('updated value Of width  is smaller than needed',  ifWidthISLess)
+                // console.log('before widthclosest', widthClosest)
+                // // widthClosest = 125   
+                //  indexOfwidth = selectorSplittedWidthArray.indexOf(widthClosest) + 1
+                //  updateWidth  = selectorSplittedWidthArray.at(indexOfwidth)
+                //  widthClosest = updateWidth
+                // console.log(indexOfwidth, 'width closest value index of ')
+                // console.log(updateWidth, 'Updated width ')
+                // console.log(widthClosest, 'after widtchclosest')
             }
 
 
@@ -102,21 +136,37 @@
 
             let finalOutput = `${heightClosest}x${widthClosest}`
 
-            console.log(widthClosest, 'widthclosest')
-            console.log(heightClosest, 'heightclostset')
+            // console.log(widthClosest, 'widthclosest')
+            // console.log(heightClosest, 'heightclostset')
 
             let indexOfselectedArray = valueWithXArray.indexOf(finalOutput)
 
+            let arrH = selectorSplittedHeightArray
+            let vish = 0
+         
+            for(let i=0; i<selectorSplittedHeightArray; i++){
+                if(arrH[i] === heightClosest){
+                    vish++
+                }
+            }
+            console.log(vish)
+
+
             if(valueWithXArray.includes(finalOutput)){
                 selector.selectedIndex =  valueWithXArray.indexOf(finalOutput) + 1
-                console.log('vissss' ,  valueWithXArray.indexOf(finalOutput) + 1)
+                // console.log('vissss' ,  valueWithXArray.indexOf(finalOutput) + 1)
             }
             else if(heightClosest > widthClosest){
+
                 console.log('height Goal AFter', heightGoalAfter , 'is bigger than ', widthGoalAfter, 'widthGoal After')
-                selector.selectedIndex = selectorSplittedHeightArray.indexOf(heightClosest) + 1
+                selector.selectedIndex = selectorSplittedHeightArray.indexOf(heightClosest) +1
+                
+
+
+
             }
             else if (heightClosest < widthClosest){
-                console.log('height Goal After', heightGoalAfter, 'is smaller than ', widthGoalAfter, 'widthgoal After')
+                // console.log('height Goal After', heightGoalAfter, 'is smaller than ', widthGoalAfter, 'widthgoal After')
                 selector.selectedIndex = selectorSplittedWidthArray.indexOf(widthClosest) + 1
             }
 
@@ -147,6 +197,8 @@
             if((height.value != '' && height.value != null) && (width.value != '' && width.value != null)){
                 if((height.value > 0 && height.value <= Math.max(...selectorSplittedHeightArray)) && (width.value > 0 && height.value <= Math.max(...selectorSplittedWidthArray)) ){
                     findClosestValue()
+                    width.style.color = 'black'
+                    height.style.color = 'black'
                 }else{
                     console.log('enter valid number')
                     width.style.color = 'red'
